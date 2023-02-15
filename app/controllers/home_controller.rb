28f3f10ade3695fa5d6308867ab3ebfc73c1e04e
freshlit <freshlitt@gmail.com>
2023-02-15 17:11:10 +0200

    no sentences coming

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
-  @api = StockQuote::Stock.new(api_key: 'pk_31d2290eac384fa597b270b0e49ab050')
-
+    @api = StockQuote::Stock.new(api_key: 'pk_31d2290eac384fa597b270b0e49ab050')
+    if params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      #puts @stock.company_name
+    end
   end
 
   def about
