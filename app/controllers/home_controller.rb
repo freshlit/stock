cde58ac272c5a7f7612fbcd31b60f64a13c76f90
freshlit <freshlitt@gmail.com>
2023-02-16 17:17:20 +0200

    Hmm..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: 'pk_31d2290eac384fa597b270b0e49ab050')
-    if params[:ticker]
+    if params[:ticker] == ""
+      @nothing = "Hey! You forgot to Enter A Symbol"
+    elsif (params[:ticker])
       @stock = StockQuote::Stock.quote(params[:ticker])
-      #puts @stock.company_name
+      puts "========== Controller, value = " + $stock.company_name + "=========="
+    else
+      puts "========== Controller, Params is NULL =========="
     end
   end
 
