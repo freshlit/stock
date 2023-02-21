a0f65afb6519f6089fde70e559641cd483744965
freshlit <freshlitt@gmail.com>
2023-02-21 12:48:54 +0200

    Until Section 7. Removed PowerDrive

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,24 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: 'pk_31d2290eac384fa597b270b0e49ab050')
-    if params[:ticker] == ""
+
+    if params[:ticker] == ''
       @nothing = "Hey! You forgot to Enter A Symbol"
+      puts "========== Controller, " + @nothing
     elsif (params[:ticker])
-      @stock = StockQuote::Stock.quote(params[:ticker])
-      puts "========== Controller, value = " + $stock.company_name + "=========="
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+        @url = StockQuote::Stock.logo(params[:ticker]).url
+
+        puts "========== Controller, found = " + @stock.company_name + "=========="
+      rescue
+        @error = "Hey! That Stock Symbol Doesn't Exist"  
+        puts "========== Controller, " + @error
+      end
     else
-      puts "========== Controller, Params is NULL =========="
+      puts "========== Controller,,,,"
     end
+    
   end
 
   def about
